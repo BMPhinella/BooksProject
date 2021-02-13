36d2979d39d4088d36fe9bbf23cf84fade8d0aec
BMPhinella <mugisabest1@gmail.com>
2021-02-13 12:58:22 +0300

    Updated interface

--- resources/views/forms/get_books.blade.php
+++ resources/views/forms/get_books.blade.php
@@ -13,7 +13,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <div class="panel-heading col-md-10 panel">
+                <div class="panel-heading col-md-12 col-lg-12 col-sm-12 panel">
                     <h5 style="text-align: center" class="panel-title">{{request()->route()->getName()}}</h5>
                 </div><!-- panel-heading -->  
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -22,28 +22,28 @@
             </div>
             <div class="modal-body">                                        
                 <div class="row">
-                    <div class="col-md-10 panel">
+                    <div class="col-lg-12 col-md-12 col-sm-12 panel">
                         <div class="card">
                         <!-- BASIC WIZARD -->
                         <form method="get" action ="/create-books"  >
             
                 <div class="form-group">
                     <label class="col-sm-4 control-label">Book Title:</label>
-                    <div class="input-group">
+                    <div class="input-group col-sm-8">
                         <input type="text" name="title" class="form-control" required />
                     </div><!-- input-group -->
                 </div><!-- form-group -->
 
                 <div class="form-group">
                     <label class="col-sm-4 control-label">Book Author:</label>
-                    <div class="input-group">
+                    <div class="input-group col-sm-8">
                         <input type="text" name="author" class="form-control" required />
                     </div><!-- input-group -->
                 </div><!-- form-group -->
 
                 <div class="form-group">
                     <label class="col-sm-4 control-label">Publication Date:</label>
-                    <div class="input-group">
+                    <div class="input-group col-sm-8">
                         <input type="date" name="publication_date" class="form-control" placeholder="mm/dd/yyyy" required />
                     </div><!-- input-group -->
                 </div><!-- form-group -->
@@ -53,7 +53,7 @@
 </div>
             <div class="modal-footer">
                 <div class="panel-footer">
-                    <div class="row">
+                    <div class="row text-center">
                         <div class="col-sm-9 col-sm-offset-3">
                             <button class="btn btn-primary mr5">Submit</button>
                             <button type="reset" class="btn btn-dark">Reset</button>
