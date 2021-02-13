4ebd397c0913ec3f61cb4d25cb4afeb10e9eea97
BMPhinella <mugisabest1@gmail.com>
2021-02-13 11:51:06 +0300

    Second commit

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/index', function () {
-    return view('index');
+Route::get('/', function () {
+    return redirect('/login');
 });
 Route::group(['middleware' => ['auth']], function () {
 
